a49a82d54c5ea2c538e7843a2d08ef13f7b16e1f
Alwin Nedrum <nedrum@gmail.com>
2022-01-10 11:30:38 +0100

    added layout css  and layout route

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	import '../layout.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		padding: 2em;
+		max-width: 800px;
+		margin: 0 auto 12em;
+	}
+</style>
